eddfb534ef4ed5345c035e46352a36ff2863a0dc
Mike Mesnier <michael.p.mesnier@gmail.com>
2026-04-22 10:27:03 -0700

    checkpoint

--- stack/patch/ufs-srweather-app-3.0.0-with-hpc-stack/devbuild.sh
+++ stack/patch/ufs-srweather-app-3.0.0-with-hpc-stack/devbuild.sh
@@ -328,10 +328,10 @@ if [ ! -f "${SRW_DIR}/modulefiles/${MODULE_FILE}.lua" ]; then
   printf "Please make sure PLATFORM and COMPILER are set correctly\n" >&2
   #usage >&2
   #exit 64
-  read -p "<Enter> to load modules directly"
   source ../../stack/load-hpc-stack 3.0.0
   module load netcdf esmf fms bacio sp crtm sfcio w3emc g2 libpng g2tmpl nemsio sigio ip hdf5
   module list
+  echo "hpc stack loaded for 3.0.0"
   read -p "<Enter> to continue build"
 fi
 
